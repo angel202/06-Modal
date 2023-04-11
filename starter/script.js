53dbe39e02dff6ec757760bcacccf6d35441f628
angel202 <angel.g5170@gmail.com>
2023-04-10 21:44:04 -0500

    Small changes to manipulate DOM
    Using classes, event hand listener to modify objects in DOM

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnOpen = document.querySelectorAll('.show-modal');
+console.log(btnOpen);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function (event) {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+
+  if (event.key === 'Escape') {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+};
+
+for (let i = 0; i < btnOpen.length; i++) {
+  btnOpen[i].addEventListener('click', openModal);
+}
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', closeModal);
